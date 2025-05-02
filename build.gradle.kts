9cd8034ec00ed3313e4f9e860cd3eadda937131e
Philipp Bobek <philipp.bobek@mailbox.org>
2025-05-02 18:42:48 +0200

    Upgrade Dokka to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@
 
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("org.jetbrains.dokka") version "1.9.20"
+    id("org.jetbrains.dokka") version "2.0.0"
     `java-library`
     `maven-publish`
     signing
